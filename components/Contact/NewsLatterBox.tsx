e06b30c495f78217ee10680807315e27359abecd
Qianru Zhang <142470034+RubyQianru@users.noreply.github.com>
2023-12-21 19:23:36 -0500

    Section Cleanup: commented out review section and ticket section

--- components/Contact/NewsLatterBox.tsx
+++ components/Contact/NewsLatterBox.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useTheme } from "next-themes";
+import { Button } from "@mui/material";
 
 const NewsLatterBox = () => {
   const { theme } = useTheme();
@@ -30,11 +31,16 @@ const NewsLatterBox = () => {
           placeholder="Enter your email"
           className="border-stroke dark:text-body-color-dark dark:shadow-two mb-4 w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:focus:border-primary dark:focus:shadow-none"
         />
-        <input
-          type="submit"
-          value="Subscribe"
-          className="shadow-submit dark:shadow-submit-dark mb-5 flex w-full cursor-pointer items-center justify-center rounded-sm bg-primary px-9 py-4 text-base font-medium text-white duration-300 hover:bg-primary/90"
-        />
+
+        <div className="flex justify-center px-6 py-3">
+        <Button
+          variant="outlined"
+          size="large"
+        >
+          Subscribe
+        </Button>
+        </div>
+        
         <p className="dark:text-body-color-dark text-center text-base leading-relaxed text-body-color">
           No spam guaranteed, So please don’t send any spam mail.
         </p>
--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,11 +1,12 @@
 import NewsLatterBox from "./NewsLatterBox";
+import { Button } from "@mui/material";
 
 const Contact = () => {
   return (
     <section id="contact" className="overflow-hidden py-16 md:py-20 lg:py-28">
       <div className="container">
         <div className="-mx-4 flex flex-wrap">
-          <div className="w-full px-4 lg:w-7/12 xl:w-8/12">
+          {/* <div className="w-full px-4 lg:w-7/12 xl:w-8/12">
             <div
               className="wow fadeInUp shadow-three dark:bg-gray-dark mb-12 rounded-sm bg-white px-8 py-11 sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
               data-wow-delay=".15s
@@ -66,14 +67,14 @@ const Contact = () => {
                     </div>
                   </div>
                   <div className="w-full px-4">
-                    <button className="shadow-submit dark:shadow-submit-dark rounded-sm bg-primary px-9 py-4 text-base font-medium text-white duration-300 hover:bg-primary/90">
-                      Submit Ticket
-                    </button>
+                    <Button  size="large" variant="contained">
+                      Submit
+                    </Button>
                   </div>
                 </div>
               </form>
-            </div>
-          </div>
+            </div> */}
+          {/* </div> */}
           <div className="w-full px-4 lg:w-5/12 xl:w-4/12">
             <NewsLatterBox />
           </div>
