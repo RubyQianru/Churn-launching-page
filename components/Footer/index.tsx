e5f9fb0e9cf74b9735eca145a04f63ff6680d539
Qianru Zhang <142470034+RubyQianru@users.noreply.github.com>
2023-12-21 19:26:06 -0500

    Initialize design

---
+++ components/Footer/index.tsx
@@ -0,0 +1,177 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <>
+      <footer
+        className="wow fadeInUp dark:bg-gray-dark relative z-10 bg-white pt-16 md:pt-20 lg:pt-24"
+        data-wow-delay=".1s"
+      >
+        <div className="container">
+          <div className="-mx-4 flex flex-wrap">
+            <div className="w-full px-4 md:w-1/2 lg:w-4/12 xl:w-5/12">
+              <div className="mb-12 max-w-[360px] lg:mb-16">
+                <Link href="/" className="mb-8 inline-block">
+                  Churn
+                </Link>
+                <p className="dark:text-body-color-dark mb-9 text-base leading-relaxed text-body-color">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Integer lobortis.
+                </p>
+                <div className="flex items-center">
+                  <a
+                    href="/"
+                    aria-label="social-link"
+                    className="dark:text-body-color-dark mr-6 text-body-color duration-300 hover:text-primary dark:hover:text-primary"
+                  >
+                  </a>
+                  <a
+                    href="/"
+                    aria-label="social-link"
+                    className="dark:text-body-color-dark mr-6 text-body-color duration-300 hover:text-primary dark:hover:text-primary"
+                  >
+                   
+                  </a>
+                  <a
+                    href="/"
+                    aria-label="social-link"
+                    className="dark:text-body-color-dark mr-6 text-body-color duration-300 hover:text-primary dark:hover:text-primary"
+                  >
+          
+                  </a>
+                  <a
+                    href="/"
+                    aria-label="social-link"
+                    className="dark:text-body-color-dark text-body-color duration-300 hover:text-primary dark:hover:text-primary"
+                  >
+
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-2/12 xl:w-2/12">
+              <div className="mb-12 lg:mb-16">
+                <h2 className="mb-10 text-xl font-bold text-black dark:text-white">
+                  Useful Links
+                </h2>
+                <ul>
+                  <li>
+                    <a
+                      href="/blogs"
+                      className="dark:text-body-color-dark mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:hover:text-primary"
+                    >
+                      Blog
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/pricing"
+                      className="dark:text-body-color-dark mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:hover:text-primary"
+                    >
+                      Pricing
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/about"
+                      className="dark:text-body-color-dark mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:hover:text-primary"
+                    >
+                      About
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-2/12 xl:w-2/12">
+              <div className="mb-12 lg:mb-16">
+                <h2 className="mb-10 text-xl font-bold text-black dark:text-white">
+                  Terms
+                </h2>
+                <ul>
+                  <li>
+                    <a
+                      href="/"
+                      className="dark:text-body-color-dark mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:hover:text-primary"
+                    >
+                      TOS
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="dark:text-body-color-dark mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:hover:text-primary"
+                    >
+                      Privacy Policy
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="dark:text-body-color-dark mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:hover:text-primary"
+                    >
+                      Refund Policy
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="w-full px-4 md:w-1/2 lg:w-4/12 xl:w-3/12">
+              <div className="mb-12 lg:mb-16">
+                <h2 className="mb-10 text-xl font-bold text-black dark:text-white">
+                  Support & Help
+                </h2>
+                <ul>
+                  <li>
+                    <a
+                      href="/contact"
+                      className="dark:text-body-color-dark mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:hover:text-primary"
+                    >
+                      Open Support Ticket
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="dark:text-body-color-dark mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:hover:text-primary"
+                    >
+                      Terms of Use
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/about"
+                      className="dark:text-body-color-dark mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:hover:text-primary"
+                    >
+                      About
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div className="h-px w-full bg-gradient-to-r from-transparent via-[#D2D8E183] to-transparent dark:via-[#959CB183]"></div>
+          <div className="py-8">
+            <p className="text-center text-base text-body-color dark:text-white">
+              Copyright{" "}
+              <a href="https://nextjstemplates.com" rel="nofollow noopener">
+                Churn
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="absolute right-0 top-14 z-[-1]">
+         
+        </div>
+        
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
---
+++ components/Testimonials/index.tsx
@@ -0,0 +1,57 @@
+import { Testimonial } from "@/types/testimonial";
+import SectionTitle from "../Common/SectionTitle";
+import SingleTestimonial from "./SingleTestimonial";
+
+const testimonialData: Testimonial[] = [
+  {
+    id: 1,
+    name: "Musharof Chy",
+    designation: "Founder @TailGrids",
+    content:
+      "Our members are so impressed. It's intuitive. It's clean. It's distraction free. If you're building a community.",
+    image: "/images/testimonials/auth-01.png",
+    star: 5,
+  },
+  {
+    id: 2,
+    name: "Devid Weilium",
+    designation: "Founder @UIdeck",
+    content:
+      "Our members are so impressed. It's intuitive. It's clean. It's distraction free. If you're building a community.",
+    image: "/images/testimonials/auth-02.png",
+    star: 5,
+  },
+  {
+    id: 3,
+    name: "Lethium Frenci",
+    designation: "Founder @Lineicons",
+    content:
+      "Our members are so impressed. It's intuitive. It's clean. It's distraction free. If you're building a community.",
+    image: "/images/testimonials/auth-03.png",
+    star: 5,
+  },
+];
+
+const Testimonials = () => {
+  return (
+    <section className="dark:bg-bg-color-dark bg-gray-light relative z-10 py-16 md:py-20 lg:py-28">
+      <div className="container">
+        <SectionTitle
+          title="What Our Users Says"
+          paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
+          center
+        />
+
+        <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 lg:grid-cols-3">
+          {testimonialData.map((testimonial) => (
+            <SingleTestimonial key={testimonial.id} testimonial={testimonial} />
+          ))}
+        </div>
+      </div>
+      
+      
+    </section>
+  );
+};
+
+export default Testimonials;
