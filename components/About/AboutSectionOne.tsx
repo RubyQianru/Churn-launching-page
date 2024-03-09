123977cde5294d287d0a8b2c76bc71de245ad05b
Vincent Campanaro <124087048+vincentcampanaro@users.noreply.github.com>
2024-03-09 12:30:02 -0500

    Update AboutSectionOne.tsx

--- components/About/AboutSectionOne.tsx
+++ components/About/AboutSectionOne.tsx
@@ -24,8 +24,8 @@ const AboutSectionOne = () => {
           <div className="-mx-4 flex flex-wrap items-center">
             <div className="w-full px-4 lg:w-1/2">
               <SectionTitle
-                title="Crafted for Startup, SaaS and Business Sites."
-                paragraph="The main ‘thrust’ is to focus on educating attendees on how to best protect highly vulnerable business applications with interactive panel discussions and roundtables."
+                title="Built for all, tailored for you."
+                paragraph="Our customized approach to personal finance ensures that the guidance you receive is uniquely tailored to your specific needs, offering real, practical solutions designed to enhance your financial well-being."
                 mb="44px"
               />
 
@@ -35,15 +35,15 @@ const AboutSectionOne = () => {
               >
                 <div className="mx-[-12px] flex flex-wrap">
                   <div className="w-full px-3 sm:w-1/2 lg:w-full xl:w-1/2">
-                    <List text="Premium quality" />
-                    <List text="Tailwind CSS" />
-                    <List text="Use for lifetime" />
+                    <List text="User-friendly interface" />
+                    <List text="Cutting-edge technology" />
+                    <List text="AI-driven and -enhanced" />
                   </div>
 
                   <div className="w-full px-3 sm:w-1/2 lg:w-full xl:w-1/2">
-                    <List text="Next.js" />
-                    <List text="Rich documentation" />
-                    <List text="Developer friendly" />
+                    <List text="Tailored, practical solutions" />
+                    <List text="Clear, actionable guidance" />
+                    <List text="Quantifiable metrics" />
                   </div>
                 </div>
               </div>
