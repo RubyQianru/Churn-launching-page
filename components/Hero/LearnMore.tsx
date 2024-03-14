5f5a21c397c15ce4a1268a9c21da5051f9146b8b
Qianru Zhang <142470034+RubyQianru@users.noreply.github.com>
2024-03-13 23:20:07 -0400

    Update learn more animation

--- components/Hero/LearnMore.tsx
+++ components/Hero/LearnMore.tsx
@@ -3,12 +3,12 @@ const LearnMore = () => {
     
     return(
         <>
-        <div className='learnMore items-center '>
-            <div className=" mb-4 text-1xl md:text-2xl font-bold leading-tight text-black dark:text-white  transition duration-300 ease-in-out">
+        <div className='learnMore items-center w-[300px]'>
+            <div className="w-[300px] mb-4 text-1xl md:text-2xl font-bold leading-tight text-black dark:text-white  transition duration-300 ease-in-out">
                 Learn More
             </div>
             <div
-                className="flex h-10 w-10 cursor-pointer items-center justify-center text-black dark:text-white transition duration-300 ease-in-out"
+                className="w-[300px] flex rotate-45 cursor-pointer items-center justify-center text-black dark:text-white transition duration-300 ease-in-out"
             >
                 <span className=" h-3 w-3 rotate-180 border-l border-t border-black dark:border-white"></span>
             </div>
