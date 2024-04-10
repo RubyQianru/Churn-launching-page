7f0a3cf358f2b865caf59bfdbd50c7c8f0ebe171
Qianru Zhang <142470034+RubyQianru@users.noreply.github.com>
2024-04-09 21:28:22 -0400

    Add scroll animation

--- components/Hero/LearnMore.tsx
+++ components/Hero/LearnMore.tsx
@@ -1,19 +1,24 @@
 import './index.css'
 const LearnMore = () => {
-    
+    let scrollDown = () => {
+        window.scrollTo({
+            top: window.scrollY + window.innerHeight,
+            behavior: 'smooth'
+        })
+
+    }
     return(
         <>
-        <div className='learnMore items-center w-[300px]'>
-            <div className="w-[300px] mb-4 text-1xl md:text-2xl font-bold leading-tight text-black dark:text-white  transition duration-300 ease-in-out">
-                Learn More
+            <div className='learnMore items-center w-[300px]' onClick={scrollDown}>
+                <div className="w-[300px] mb-4 text-1xl md:text-2xl font-bold leading-tight text-black dark:text-white  transition duration-300 ease-in-out">
+                    Learn More
+                </div>
+                <div
+                    className="w-[300px] flex rotate-45 cursor-pointer items-center justify-center text-black dark:text-white transition duration-300 ease-in-out"
+                >
+                    <span className=" h-3 w-3 rotate-180 border-l border-t border-black dark:border-white"></span>
+                </div>
             </div>
-            <div
-                className="w-[300px] flex rotate-45 cursor-pointer items-center justify-center text-black dark:text-white transition duration-300 ease-in-out"
-            >
-                <span className=" h-3 w-3 rotate-180 border-l border-t border-black dark:border-white"></span>
-            </div>
-
-        </div>
             
         </>
     )
